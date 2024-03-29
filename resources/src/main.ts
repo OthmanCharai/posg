2933e6bb0c0c1b96f3fe691cb2b45aaed749d071
bj-anas <93322743+bj-anas@users.noreply.github.com>
2024-03-29 23:17:09 +0000

    install icon package

--- resources/src/main.ts
+++ resources/src/main.ts
@@ -2,14 +2,16 @@ import { createApp } from "vue";
 import { createPinia } from 'pinia';
 import { router } from "@/src/router";
 import VueSweetalert2 from "vue-sweetalert2";
+import VueFeather from "vue-feather";
 import App from "./App.vue";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "sweetalert2/dist/sweetalert2.min.css";
 import "@/src/assets/scss/main.scss";
-import "@/src/assets/css/style.css";
 
 const app = createApp(App);
+
+app.component(VueFeather.name, VueFeather);
 
 app.use(VueSweetalert2);
 app.use(createPinia());
