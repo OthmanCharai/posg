f32e24f33ea33d1e15a88043ede840c411ecae5a
bj-anas <93322743+bj-anas@users.noreply.github.com>
2024-03-31 18:40:57 +0000

    add deepClone object

---
+++ resources/src/utils/object.ts
@@ -0,0 +1,10 @@
+/**
+* Creates a deep clone of the specified object.
+*/
+export function deepClone<T>(data: unknown): T {
+  if (typeof data !== 'object' || data === null) {
+    return data as T;
+  }
+
+  return JSON.parse(JSON.stringify(data)) as T;
+}
