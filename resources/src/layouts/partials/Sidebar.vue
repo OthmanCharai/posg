7b4b86e8eac0b0abca62ef869f30875e3d9c1cbe
bj-anas <93322743+bj-anas@users.noreply.github.com>
2024-04-01 02:52:07 +0000

    set sidebar

--- resources/src/layouts/partials/Sidebar.vue
+++ resources/src/layouts/partials/Sidebar.vue
@@ -1,38 +1,24 @@
+<script setup lang="ts">
+import { PerfectScrollbar } from "vue3-perfect-scrollbar";
+import "vue3-perfect-scrollbar/dist/vue3-perfect-scrollbar.css";
+import UserMenu from "./UserMenu.vue";
+
+const settings = {
+  suppressScrollX: true,
+};
+
+const scrollHandle: () => void = () => {};
+</script>
+
 <template>
-    <div class="sidebar" id="sidebar">
-      <div class="sidebar-inner slimscroll">
-        <perfect-scrollbar
-          class="scroll-area"
-          :settings="settings"
-          @ps-scroll-y="scrollHanle"
-        >
-          <div id="sidebar-menu" class="sidebar-menu">
-            <user-menu></user-menu>
-          </div>
-        </perfect-scrollbar>
-      </div>
+  <div class="sidebar" id="sidebar">
+    <div class="sidebar-inner slimscroll">
+      <PerfectScrollbar class="scroll-area" :settings="settings" @ps-scroll-y="scrollHandle()">
+        <div id="sidebar-menu" class="sidebar-menu">
+          <UserMenu />
+        </div>
+      </PerfectScrollbar>
     </div>
-    <collapsed-sidebar></collapsed-sidebar>
-    <horizontal-sidebar></horizontal-sidebar>
-  </template>
-  
-  <script>
-  import { PerfectScrollbar } from "vue3-perfect-scrollbar";
-  import "vue3-perfect-scrollbar/dist/vue3-perfect-scrollbar.css";
-  export default {
-    components: {
-      PerfectScrollbar,
-    },
-    data() {
-      return {
-        settings: {
-          suppressScrollX: true,
-        },
-      };
-    },
-    methods: {
-      scrollHanle() {},
-    },
-  };
-  </script>
-  
+  </div>
+  <collapsed-sidebar></collapsed-sidebar>
+</template>
