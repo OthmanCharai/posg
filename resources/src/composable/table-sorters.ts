bc2c7c4a80f578e4c488b519a7e4714615b20927
bj-anas <93322743+bj-anas@users.noreply.github.com>
2024-04-08 02:00:37 +0000

    remove comment

--- resources/src/composable/table-sorters.ts
+++ resources/src/composable/table-sorters.ts
@@ -1,7 +1,5 @@
 export function createLengthBasedSorter <T>(data: T[], key: keyof T) {
   return (a: T, b: T) => {
-    // Assuming the key always refers to a property whose value is a string or has a toString method.
-    // You might need additional checks if some properties could be non-string or lack toString.
     const aValue = String(a[key]);
     const bValue = String(b[key]);
     if (aValue === String(data[0][key]) || bValue === String(data[0][key])) {
@@ -11,7 +9,6 @@ export function createLengthBasedSorter <T>(data: T[], key: keyof T) {
   };
 };
 
-// Generic numeric sorter function
 export function createNumericSorter <T>(key: keyof T) {
   return (a: T, b: T) => {
     const aValue = Number(a[key]);
@@ -20,7 +17,6 @@ export function createNumericSorter <T>(key: keyof T) {
   };
 };
 
-// Generic date sorter function
 export function createDateSorter <T>(key: keyof T) {
   return (a: T, b: T) => {
     const aValue = new Date(a[key] as unknown as string).getTime();
