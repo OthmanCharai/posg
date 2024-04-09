1b3215737d1871e8bde9e21de0ab768c717b2249
bj-anas <93322743+bj-anas@users.noreply.github.com>
2024-04-09 23:36:17 +0000

    create 404 page

--- resources/src/pages/errors/404.vue
+++ resources/src/pages/errors/404.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1>Page non trouvé 404 !!</h1>
+  <div class="wrong-page-wrapper">
+    <a-result status="404" title="404" sub-title="Désolé, la page que vous avez visitée n'existe pas.">
+      <template #extra>
+        <a
+          href="/"
+          class="btn btn-primary"
+          >
+          Retour à l'accueil
+        </a>
+      </template>
+    </a-result>
+  </div>
 </template>
+
+<style scoped>
+  .wrong-page-wrapper {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
