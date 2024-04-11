c74296e3ae29575db93b70e6b75cd0ebb833d085
Anas <93322743+bj-anas@users.noreply.github.com>
2024-04-11 23:31:54 +0000

    setup modal (#37)

--- resources/src/pages/admin/users/Users.vue
+++ resources/src/pages/admin/users/Users.vue
@@ -4,6 +4,7 @@ import type { Users } from '@common/types/users';
 import type { PaginationMetadata } from '@common/types/global/pagination';
 import { extractPaginatorObject } from '@utils/pagination';
 import { lengthSorter } from '@/src/composables/table-sorters';
+import { SelectProps } from 'ant-design-vue/es/vc-select/Select';
 
 const { request, response, loading } = useAxios();
 
@@ -49,6 +50,29 @@ const getUsersList = async (page: number = 1) => {
   }
 }
 
+const showModal = ref(false);
+const data = ref({
+  userName: '',
+})
+
+const options = ref<SelectProps['options']>([
+  { value: 'jack', label: 'Jack' },
+  { value: 'lucy', label: 'Lucy' },
+  { value: 'tom', label: 'Tom' },
+]);
+
+const filterOption = (input: string, option: any) => {
+  return option.value.toLowerCase().indexOf(input.toLowerCase()) >= 0;
+};
+
+const handleSubmission = () => {
+  console.log('hello');
+};
+
+const fileList = ref([]);
+const imgLoader = ref<boolean>(false);
+const imageUrl = ref<string>('');
+
 onMounted(async () => {
   await getUsersList();
 })
@@ -60,8 +84,7 @@ onMounted(async () => {
       <a
         href="javascript:void(0);"
         class="btn btn-added color"
-        data-bs-toggle="modal"
-        data-bs-target="#view-notes"
+        @click="showModal = true"
         >
         <vue-feather type="plus-circle" class="me-2"></vue-feather>
         Ajouter
@@ -95,4 +118,82 @@ onMounted(async () => {
       </DataTable>
     </div>
   </div>
+
+  <ModalWrapper title="Nouveau utilisateur" v-model:open="showModal" @submit="handleSubmission" width="800px">
+    <a-divider class="!text-xl">Données personnelles</a-divider>
+    <section class="grid grid-cols-2 gap-4">
+      <div class="grid gap-4">
+        <a-form-item
+          validate-status=""
+          help=""
+        >
+          <a-input addonBefore="Nom d'utilisateur" v-model:value="data.userName" />
+        </a-form-item>
+
+        <a-form-item
+          validate-status=""
+          help=""
+        >
+          <a-input addonBefore="Mot de passe" v-model:value="data.userName" />
+        </a-form-item>
+
+        <a-form-item
+          validate-status=""
+          help=""
+        >
+          <a-input addonBefore="Nom" v-model:value="data.userName" />
+        </a-form-item>
+
+        <a-form-item
+          validate-status=""
+          help=""
+        >
+          <a-input addonBefore="Prenom" v-model:value="data.userName" />
+        </a-form-item>
+      </div>
+      <div>
+        <a-form-item
+          validate-status=""
+          help=""
+        >
+          <a-textarea placeholder="Address" class="h-[88px!important] mb-4" v-model:value="data.userName" />
+        </a-form-item>
+
+        <a-form-item
+          validate-status=""
+          help=""
+        >
+          <a-input addonBefore="Tel" v-model:value="data.userName" />
+        </a-form-item>
+      </div>
+    </section>
+    <section class="middle-section">
+      <a-divider class="!text-xl">Roles</a-divider>
+      <a-select
+        v-model:value="data.userName"
+        show-search
+        style="width: 100%"
+        placeholder="Select a person"
+        :options="options"
+        :filter-option="filterOption"
+      ></a-select>
+    </section>
+    <section class="last-section">
+      <a-divider class="!text-xl">Image</a-divider>
+      <a-upload
+        v-model:file-list="fileList"
+        name="avatar"
+        list-type="picture-card"
+        class="avatar-uploader"
+        :show-upload-list="false"
+      >
+        <img v-if="imageUrl" :src="imageUrl" alt="avatar" />
+        <div v-else>
+          <loading-outlined v-if="imgLoader"></loading-outlined>
+          <plus-outlined v-else></plus-outlined>
+          <div class="ant-upload-text">Upload</div>
+        </div>
+      </a-upload>
+    </section>
+  </ModalWrapper>
 </template>
