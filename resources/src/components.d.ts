169b2a1e84e5e7e1b0317800bb661ff13036f9e8
Anas <93322743+bj-anas@users.noreply.github.com>
2024-04-11 18:49:33 +0000

    setup tailwind (#36)

--- resources/src/components.d.ts
+++ resources/src/components.d.ts
@@ -10,7 +10,6 @@ declare module 'vue' {
     AApp: typeof import('ant-design-vue/es')['App']
     AConfigProvider: typeof import('ant-design-vue/es')['ConfigProvider']
     AResult: typeof import('ant-design-vue/es')['Result']
-    ASpace: typeof import('ant-design-vue/es')['Space']
     ATable: typeof import('ant-design-vue/es')['Table']
     DataTable: typeof import('./components/tables/DataTable.vue')['default']
     Filter: typeof import('./components/Filter.vue')['default']
---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/src/**/*.{vue,js,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
