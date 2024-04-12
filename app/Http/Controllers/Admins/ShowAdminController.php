fdece7d60dcb309b32e6d7b9e68900ff9a9f2f71
Othman-Charai <150145756+Othman-Charai@users.noreply.github.com>
2024-04-12 16:00:28 +0000

    add roles

--- app/Http/Controllers/Admins/ShowAdminController.php
+++ app/Http/Controllers/Admins/ShowAdminController.php
@@ -3,16 +3,24 @@
 namespace App\Http\Controllers\Admins;
 
 use App\Http\Controllers\Controller;
+use App\Http\Transformers\RoleTransformer;
 use App\Http\Transformers\UserTransformer;
 use App\src\Models\User\User;
+use App\src\Services\AdminRole\AdminRoleServiceInterface;
 
 class ShowAdminController extends Controller
 {
+    public function __construct(private readonly AdminRoleServiceInterface $adminRoleService)
+    {
+        parent::__construct();
+    }
+
     public function __invoke(User $user): \Illuminate\Http\JsonResponse
     {
         return $this->response->withArray(
             [
-                'user' => UserTransformer::transform($user),
+                'user'  => UserTransformer::transform($user),
+                'roles' => transform_collection($this->adminRoleService->get(), RoleTransformer::class)->toArray(),
             ]
         );
     }
