22d0d2d426eb8d7f9f0009c368834e275709277f
bj-anas <93322743+bj-anas@users.noreply.github.com>
2024-04-24 00:06:29 +0100

    create taxe list

--- resources/src/pages/admin/administration/partials/taxes/TaxesList.vue
+++ resources/src/pages/admin/administration/partials/taxes/TaxesList.vue
@@ -2,5 +2,43 @@
 </script>
 
 <template>
-  <span>Content of Tab Pane 3</span>
+  <div class="overflow-hidden bg-white shadow-md sm:rounded-lg">
+    <div class="px-4 py-6 sm:px-6 flex justify-between">
+      <div class="flex gap-2">
+        <h3 class="text-base font-semibold leading-7 text-gray-900">Applicant Information</h3>
+        <a-button type="default" size="middle" class="!p-1">
+          <template #icon>
+            <vue-feather type="edit" class="w-4"></vue-feather>
+          </template>
+        </a-button>
+      </div>
+      <a-button type="default" shape="circle" size="small" class="!py-1 !px-1 !w-[28px]">
+        <template #icon>
+          <vue-feather type="x" class="w-4"></vue-feather>
+        </template>
+      </a-button>
+    </div>
+    <div class="border-t border-gray-100 p-4">
+      <DataTable
+        :columns="[]"
+        :data="[]"
+        :fetched-data="{}"
+        :loading="false"
+      >
+        <template #bodyCell="{column, record}">
+          <template v-if="column.key === 'action'">
+            <td class="action-table-data">
+              <button class="action-button edit" @click="">
+                <vue-feather type="edit"></vue-feather>
+              </button>
+              <button class="action-button delete" @click="">
+                <vue-feather type="trash-2"></vue-feather>
+              </button>
+            </td>
+          </template>
+        </template>
+      </DataTable>
+    </div>
+  </div>
 </template>
+
