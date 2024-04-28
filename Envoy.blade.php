cb18bf9ac9155d867d480c71c33ee16daa1e2405
Othman-Charai <150145756+Othman-Charai@users.noreply.github.com>
2024-04-28 15:49:00 +0100

    laravel envoy

---
+++ Envoy.blade.php
@@ -0,0 +1,50 @@
+@servers(['web' => 'deployer@93.127.203.1'])
+
+@setup
+$repository = 'git@github.com:OthmanCharai/posg.git';
+$releases_dir = '/var/www/html/posg/releases';
+$app_dir = '/var/www/html/posg';
+$release = date('YmdHis');
+$new_release_dir = $releases_dir . '/' . $release;
+$password = 'password';
+@endsetup
+
+@story('deploy')
+clone_repository
+run_docker
+run_composer
+npm_build
+update_symlinks
+@endstory
+
+@task('clone_repository')
+echo 'Cloning repository'
+[ -d {{ $releases_dir }} ] || mkdir {{ $releases_dir }}
+git clone --depth 1 {{ $repository }} {{ $new_release_dir }}
+cd {{ $new_release_dir }}
+git reset --hard {{ $commit }}
+@endtask
+
+@task('run_docker')
+echo "Starting deployment ({{ $release }})"
+cd {{ $new_release_dir }}
+sudo docker-compose up -d
+@endtask
+
+@task('run_composer')
+echo "Running Composer"
+cd {{ $new_release_dir }}
+sudo docker exec -it posg_laravel.test_1 composer install
+@endtask
+
+@task('npm_build')
+echo "Running npm build"
+cd {{ $new_release_dir }}
+sudo docker exec -it posg_laravel.test_1 npm run build
+@endtask
+
+@task('update_symlinks')
+echo 'Linking current release'
+ln -nfs {{ $new_release_dir }} {{ $app_dir }}/current
+echo "{{ $password }}" | sudo -S chmod 777 -R {{ $app_dir }}
+@endtask
