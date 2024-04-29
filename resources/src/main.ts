c1fd9a5100614862dcc80bf00c5549d56cfe557e
Anas <93322743+bj-anas@users.noreply.github.com>
2024-04-29 03:01:39 +0100

    clean code from bootstrap (#55)

--- resources/src/main.ts
+++ resources/src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from 'pinia';
 import { router } from "@/src/router";
-import { BootstrapVue3, BToastPlugin } from "bootstrap-vue-3";
 import VueSweetalert2 from "vue-sweetalert2";
 import VueFeather from "vue-feather";
 import.meta.glob(['@/src/assets/img/logo/favicon.ico']);
@@ -12,7 +11,4 @@ app.component(VueFeather.name, VueFeather);
 
 app.use(VueSweetalert2);
 app.use(createPinia());
-app
-  .use(BootstrapVue3)
-  .use(BToastPlugin);
 app.use(router).mount("#app");
