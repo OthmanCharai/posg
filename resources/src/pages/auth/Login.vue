cb39cd1e0ba3d17f5982bd7ff9bdf5a17b54e81d
Anas <93322743+bj-anas@users.noreply.github.com>
2024-05-01 00:44:15 +0100

    fix login page css (#57)

--- resources/src/pages/auth/Login.vue
+++ resources/src/pages/auth/Login.vue
@@ -1,25 +1,22 @@
 <script setup lang="ts">
-import { Form, Field } from "vee-validate";
 import { useAxios, route } from "@/src/utils/axios-helper";
 import { useRouter } from "vue-router";
-import { getErrorMessage, hasError } from "@utils/error-handler";
+import { getErrorMessage, isError, clearError } from "@utils/error-handler";
 import { useAuthStore } from '@stores/auth.store';
 import { Toast } from "@utils/toast";
+import { LoadingOutlined } from '@ant-design/icons-vue';
+
 
 const authStore = useAuthStore();
 const { request, loading, response } = useAxios();
 const router = useRouter();
-const showPassword = ref(false);
-
-// Methods
-const toggleShow = () => {
-  showPassword.value = !showPassword.value;
-};
 
 const data = ref({
   email: '',
   password: '',
 })
+
+const checked = ref(false);
 
 const onSubmit = async() => {
   await request({
@@ -46,7 +43,7 @@ const onSubmit = async() => {
             <div class="login-logo">
               <img src="@/src/assets/img/logo/logo-login.png" alt="logo" />
             </div>
-            <Form @submit="onSubmit">
+            <a-form layout="vertical" @submit="onSubmit">
               <div class="login-userset">
                 <div class="login-userheading">
                   <h3>Se connecter</h3>
@@ -54,67 +51,46 @@ const onSubmit = async() => {
                     Connectez-vous au tableau de bord avec votre e-mail et code d'accès.
                   </h4>
                 </div>
-                <div class="form-login">
-                  <label class="form-label">Adresse e-mail</label>
-                  <div class="form-addons">
-                    <Field
-                      name="email"
-                      type="text"
-                      v-model="data.email"
-                      class="form-control"
-                      :class="{ 'is-invalid': hasError('email') }"
-                    />
-                    <div class="invalid-feedback">
-                      {{ getErrorMessage('email') }}
-                    </div>
-                    <vue-feather type="mail" size="16" :class="{'hide-icon': hasError('email')}"></vue-feather>
+                <div class="mb-4">
+                  <a-form-item
+                    label="Address e-mail"
+                    :validate-status="isError('email')"
+                    :help="getErrorMessage('email')"
+                  >
+                    <a-input size="large" type="email" v-model:value="data.email" @change="clearError('email')">
+                      <template #suffix>
+                        <vue-feather type="mail" stroke="#8c8c8c" size="16"></vue-feather>
+                      </template>
+                    </a-input>
+                  </a-form-item>
+                </div>
+                <div class="mb-4">
+                  <a-form-item
+                    label="Mot de passe"
+                    :validate-status="isError('password')"
+                    :help="getErrorMessage('password')"
+                  >
+                    <a-input-password size="large" v-model:value="data.password" @change="clearError('password')"/>
+                  </a-form-item>
+                </div>
+                <div class="form-login flex justify-between">
+                  <a-checkbox class="max-w-max" v-model:checked="checked">Se souvenir de moi</a-checkbox>
+                  <div class="">
+                    <router-link class="forgot-link" to="/forgot-password-3">
+                      Mot de passe oublié ?
+                    </router-link>
                   </div>
                 </div>
-                <div class="form-login">
-                  <label>Mot de passe</label>
-                  <div class="pass-group">
-                    <Field
-                      name="password"
-                      :type="showPassword ? 'text' : 'password'"
-                      v-model="data.password"
-                      class="form-control pass-input mt-2"
-                      :class="{ 'is-invalid': hasError('password') }"
-                    />
-                    <span @click="toggleShow" class="toggle-password">
-                      <vue-feather v-if="showPassword" type="eye" size="16" :class="{'hide-icon': hasError('passowrd')}"></vue-feather>
-                      <vue-feather v-else type="eye-off" size="16" :class="{'hide-icon': hasError('password')}"></vue-feather>
-                    </span>
-                    <div class="invalid-feedback">
-                      {{ getErrorMessage('password') }}
-                    </div>
-                  </div>
-                </div>
-                <div class="form-login authentication-check">
-                  <div class="row align-items-center">
-                    <div class="col-6">
-                      <div class="custom-control custom-checkbox">
-                        <label class="checkboxs ps-4 mb-0 pb-0 line-height-1">
-                          <input type="checkbox" />
-                          <span class="checkmarks"></span>Se souvenir de moi
-                        </label>
-                      </div>
-                    </div>
-                    <div class="col-6 text-end">
-                      <router-link class="forgot-link" to="/forgot-password-3">
-                        Mot de passe oublié ?
-                      </router-link>
-                    </div>
-                  </div>
-                </div>
-                <div class="form-login">
-                  <button :disabled="loading" class="btn btn-login" type="submit">Se connecter</button>
+                <div>
+                  <a-button size="large" type="primary" htmlType="submit">
+                    <template v-if="loading" #icon><LoadingOutlined /></template>
+                    <span v-if="loading === false"> Se connecter</span>
+                  </a-button>
                 </div>
               </div>
-            </Form>
+            </a-form>
           </div>
-          <div
-            class="my-4 d-flex justify-content-center align-items-center copyright-text"
-          >
+          <div class="my-4 flex justify-center items-center copyright-text">
             <p>
               Droit d'auteur &copy; {{ new Date().getFullYear() }} Piston Gabon. Tous droits réservés.
             </p>
@@ -125,8 +101,19 @@ const onSubmit = async() => {
   </div>
 </template>
 
-<style scoped>
-.hide-icon {
-  display: none;
+<style scoped lang="scss">
+:deep(.ant-input-affix-wrapper-lg) {
+  border-radius: 0 !important;
+}
+:deep(.ant-btn.ant-btn-lg) {
+  border-radius: 0 !important;
+  width: 100% !important;
+  height: 47.14px !important;
+
+  &:is(:hover, :active) {
+    background: white !important;
+    border: 1px solid #001656;
+    color: #001656 !important;
+  }
 }
 </style>
