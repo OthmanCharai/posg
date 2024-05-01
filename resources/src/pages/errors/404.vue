98de853299047e9941aa7265e6b4b075ea794ad0
bj-anas <93322743+bj-anas@users.noreply.github.com>
2024-05-01 17:29:03 +0100

    fix 404 link

--- resources/src/pages/errors/404.vue
+++ resources/src/pages/errors/404.vue
@@ -2,10 +2,12 @@
   <div class="wrong-page-wrapper">
     <a-result status="404" title="404" sub-title="Désolé, la page que vous avez visitée n'existe pas.">
       <template #extra>
-        <a-button type="primary" href="/">
-          <vue-feather :size="16" type="arrow-left"></vue-feather>
-          <span>Retour à l'accueil</span>
-        </a-button>
+        <router-link to="/dashboard" class="w-full">
+          <a-button type="primary" class="w-full">
+            <vue-feather :size="16" type="arrow-left"></vue-feather>
+            <span>Retour à l'accueil</span>
+          </a-button>
+        </router-link>
       </template>
     </a-result>
   </div>
