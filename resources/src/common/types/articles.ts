04d293b5aaea541e65fbaed751fb2828c17d3a18
bj-anas <93322743+bj-anas@users.noreply.github.com>
2024-05-05 02:41:21 +0100

    add types

---
+++ resources/src/common/types/articles.ts
@@ -0,0 +1,26 @@
+export interface ArticleInfo {
+  id?: string,
+  code_bare: string,
+  supplier_id: string,
+  name: string,
+  emplacement: string,
+  purchase_price: string,
+  wholesale_price: string,
+  retail_price: string,
+  last_sale_price: string,
+  stock_type: number,
+  brand_id: string,
+  article_category_id: string,
+  image: File | string | null | undefined,
+  description: string,
+  compatibilities: string[],
+}
+
+export interface ArticleStock {
+  depot_id: string,
+  quantity: string,
+}
+
+export interface ArticleCompatibility {
+  name: string,
+}
